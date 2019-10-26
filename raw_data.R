6faafbc3aff1202574241b5383011995ff3be37e
Ashokkumar Sharma <ashurocks1996@gmail.com>
2019-10-25 21:56:24 -0400

    data cleaning car1

--- raw_data.R
+++ raw_data.R
@@ -1,7 +1,7 @@
 #install.packages("stringr")
 library(stringr)
 
-#
+ #
 data <- read.csv("../Pecan_70_Houses_2017.csv", header = TRUE, sep = ",")
 #
 raw_data <- data
@@ -32,3 +32,20 @@ raw_data <- raw_data[-which(raw_data$dataid == ""), ]
 #
 unique(raw_data$dataid)
 length(unique(raw_data$dataid))         #
+
+range(raw_data$air1)
+class(raw_data$dataid)
+raw_data$dataid <- as.integer(raw_data$dataid)
+
+length(unique(raw_data$dataid[which(raw_data$car1 != 0)]))
+which(raw_data$dataid == "7024")
+
+length(unique(raw_data$dataid[which(raw_data$gen != 0)]))
+
+length(which(is.na(raw_data$car1)))
+
+raw_data[is.na(raw_data)] <- 0
+
+length(is.na(raw_data))
+
+
